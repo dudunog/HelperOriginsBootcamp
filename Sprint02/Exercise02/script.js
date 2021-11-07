fc4250fc99734fb88acea1033ca1f7b000d59e65
Eduardo Nogueira <eduardodudunog@gmail.com>
2021-11-06 23:29:14 -0300

    Adding the solution to the problem from exercise 2

--- Sprint02/Exercise02/script.js
+++ Sprint02/Exercise02/script.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var list_1 = require("./list");
+var List_1 = require("./list");
 var Person_1 = require("./Person");
 var PersonImperative_1 = require("./PersonImperative");
 //get bio - functional
-console.log(Person_1.Person.getBio(3));
+console.log(Person_1.Person.getBio(1));
 //get bio - imperative
-console.log(PersonImperative_1.PersonImperative.getBio(1));
+console.log(PersonImperative_1.PersonImperative.getBio(4));
 //get name - functional
-console.log(Person_1.Person.getName(1));
+console.log(Person_1.Person.getName(2));
 //get name - functional
-console.log(PersonImperative_1.PersonImperative.getName(1));
+console.log(PersonImperative_1.PersonImperative.getName(3));
